7602213bb6d700949dc299825455b8aedbf53cbc
andriusmw <andriusmw@yahoo.es>
2024-12-08 18:40:09 +0100

    lazy load router

---
+++ src/app/auth/auth.routes.ts
@@ -0,0 +1,9 @@
+import { Route } from "@angular/router";
+import { RegisterComponent } from "./authcomponents/register/register.component";
+
+export const registerRoutes: Route[] = [
+  {
+    path: '',
+    component: RegisterComponent,
+  },
+]
