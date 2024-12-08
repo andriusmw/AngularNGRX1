a515c6ed0afccec6f30feb6bf3faca6cfda568e5
andriusmw <andriusmw@yahoo.es>
2024-12-08 19:26:48 +0100

    Reactive Forms

--- src/app/auth/authcomponents/register/register.component.ts
+++ src/app/auth/authcomponents/register/register.component.ts
@@ -1,8 +1,26 @@
 import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
+  imports: [ReactiveFormsModule]
 })
-  export class RegisterComponent {}
+  export class RegisterComponent {
+    form = this.fb.nonNullable.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password:['', Validators.required],
+    })
+    constructor(private fb: FormBuilder) {}
+
+
+    onSubmit() {
+      console.log('form', this.form.getRawValue())
+    }
+
+
+
+  }
